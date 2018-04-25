3d58382e01d936cd13f74ace2d71bc5e49324e61
Richard Zou <zou3519@gmail.com>
2018-04-25 09:55:38 -0600

    git aliases

---
+++ git-aliases.sh
@@ -0,0 +1,6 @@
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.subup "submodule update --init"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
